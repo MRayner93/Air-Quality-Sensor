68191d1669b79d42004fd464b5862b8b2ad00a8b
MRayner93 <112480580+MRayner93@users.noreply.github.com>
2023-03-09 11:19:25 +0100

    alpha1

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
-import machine
+"""
+Merlin Rayner, ETS22, 22.11.2022
+
+Version 1.0, 09.03.2022
+
+Hardware:
+- 1*ESP32_lora_Display
+- 3*Widerstand 220Ohm
+- 1*LEDs Gruen
+- 1*LEDs Gelb
+- 1*LEDs Rot
+- 1*MQ135 Gassensor
+- 1*Breadboard
+- Verdrahtungsbruecken
+
+Gassensor = Pin 36
+Led Rot = Pin 13
+Led Gelb =
+Led Grün =
+
+Bibliotheken:
+-machine (integriert in Micropython)
+-time (integriert in Micropython)
+-math (integriert in Micropython)
+"""
+#-------------------------- Bibliotheken ------------------------------
+from machine import Pin, ADC
 import time
-
-# Konfiguration des MQ135 Sensors
-pin = machine.Pin(4, machine.Pin.IN, machine.Pin.PULL_UP)
-RL_VALUE = 20000  # Widerstand des Lastwiderstands
-RO_CLEAN_AIR_FACTOR = 3.8  # Empirischer Faktor zur Berechnung des Widerstands des Sensors bei sauberer Luft
-
-# Funktion zur Berechnung des Widerstands des Sensors
-def get_sensor_resistance():
-    value = 0
-    for i in range(200):
-        value += pin.value()
-        time.sleep_ms(1)
-    value = (value/200) * 3.3 / 4096.0  # Berechnung der Spannung
-    sensor_volt = value / (1.0 - value/3.3) * RL_VALUE  # Berechnung der Spannung am Sensor
-    sensor_resistance = sensor_volt / RO_CLEAN_AIR_FACTOR  # Berechnung des Widerstands des Sensors
-    return sensor_resistance
-
-# Endlosschleife zum Ausgeben der Werte
+#---------------------------- Programm --------------------------------
 while True:
-    sensor_resistance = get_sensor_resistance()
-    print("MQ135 Sensor Widerstand: {:.2f} kOhm".format(sensor_resistance/1000))
+    adc = ADC(Pin(36))
+    adc.atten(ADC.ATTN_11DB) # Einstellen des ADC-Verstärkungsfaktors auf 11 dB
+
+    # Lesen Sie den analogen Wert
+    analogwert = adc.read()
+
+    # Drucken Sie den Wert auf der Konsole
+    print(analogwert)
+    
+    # Umwandeln des Analogwerts in eine Spannung
+   # spannung = analogwert * 0.0078125
+
+    # Drucken Sie die Spannung auf der Konsole
+   # print(spannung)
     time.sleep(1)
