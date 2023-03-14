e533bc36d258b05b505be1e20b70d2248c9669c0
MRayner93 <112480580+MRayner93@users.noreply.github.com>
2023-03-14 12:50:53 +0100

    v1.0

---
+++ boot.py
@@ -0,0 +1,126 @@
+#-------------------------- Bibliotheken ------------------------------
+# PINs, SoftI2C und analoge PINs
+from machine import Pin, ADC, SoftI2C
+# Zeitfunktionen
+import time
+# JSON Datenformat
+import json
+# MQTT-Client
+from umqtt.simple import MQTTClient
+# Netzwerkfunktion
+import network
+# OLED-Screen
+import ssd1306
+# Funktionen
+import funktionen
+
+#------------------------ Pin initialisierung --------------------------
+# grüne LED auf Pin 13
+led_gruen = Pin (13, Pin.OUT)
+# gelbe LED auf Pin 17
+led_gelb = Pin (17, Pin.OUT)
+# rote LED auf Pin 23
+led_rot = Pin (23, Pin.OUT)
+# Buzzer auf Pin 22
+buzzer = Pin (22, Pin.OUT)
+# I2C-Takt auf Pin 15 - OLED
+softi2c_scl = Pin(15, Pin.OUT, Pin.PULL_UP)
+# I2C-Daten auf Pin 4 - OLED
+softi2c_sda = Pin(4, Pin.OUT, Pin.PULL_UP)
+# Display Reset auf Pin 16 - OLED
+d_rst = Pin(16,Pin.OUT)
+# Analogwert vom Sensor auf Pin 36
+analog = ADC(Pin(36))
+
+#------------------------ Analog Pin Verstärkung -----------------------
+# Analog Digital Converter Verstärkung um 11 dB
+analog.atten(ADC.ATTN_11DB)
+
+#------------------------ Display initialisierung ----------------------
+# I2C-Objekt wird erstellt
+softi2c = SoftI2C(scl=softi2c_scl, sda=softi2c_sda)
+# OLED-Display Reset-Pin wird auf 0 gesetzt
+d_rst.value(0)
+# Es wird 0.2 Sekunden gewartet
+time.sleep(0.2)
+# OLED-Display Reset-Pin wird auf 1 gesetzt
+d_rst.value(1)
+# Display-Objekt wird erstellt und die Größe des Displays wird festgelegt
+display = ssd1306.SSD1306_I2C(128, 64, softi2c)
+
+#----------------------- LED und Buzzer Testen -------------------------
+# Buzzer und alle LEDs gehen an
+buzzer.on()
+led_gruen.on()
+led_gelb.on()
+led_rot.on()
+
+# Es wird 0.2 Sekunden gewartet
+time.sleep(0.2)
+
+# Buzzer und alle LEDs gehen aus
+led_gruen.off()
+led_gelb.off()
+led_rot.off()
+buzzer.off()
+
+#-------------------------- WLAN Verbindung ---------------------------
+# Auf dem Display wird die Initialisierung für das WLAN angezeigt
+display.fill(0)
+display.text("Initialisiere", 0, 0)
+display.text("WLAN...", 0, 15)
+display.show()
+
+# WLAN-Name
+WIFI_SSID = "BZTG-IoT"
+# WLAN-Passwort
+WIFI_PASSWORD = "WerderBremen24"
+# WLAN-Client erzeugen
+wlan = network.WLAN(network.STA_IF)
+# WLAN ausschalten
+wlan.active(False)
+# WLAN einschalten
+wlan.active(True)
+# Warten, damit WLAN eingeschaltet ist
+time.sleep(0.5)
+# Verbindung zum WLAN herstellen
+wlan.connect(WIFI_SSID,WIFI_PASSWORD)
+# Solange warten, bis Verbindung hergestellt
+while not wlan.isconnected():
+    pass
+
+# Auf dem Display wird der Erfolg der Verbindung angezeigt
+display.fill(0)
+display.text("Erfolgreich!", 0, 0)
+display.show()
+time.sleep(2)
+
+# In der Konsole werden SSDID, IP und die Subnetzmaske angezeigt
+networkList = wlan.ifconfig()
+print("SSID:", wlan.config("essid"))
+print("IP:", networkList[0])
+print("SUBNET:", networkList[1])
+
+#------------------------------ MQTT ----------------------------------
+# Auf dem Display wird die Initialisierung für MQTT angezeigt
+display.fill(0)
+display.text("Initialisiere", 0, 0)
+display.text("MQTT...", 0, 15)
+display.show()
+
+# MQTT Broker
+MQTT_SERVER = "192.168.1.133"
+# MQTT Client ID
+CLIENT_ID = "MQTT_MR"
+# MQTT Parameter
+mqttClient = MQTTClient(CLIENT_ID, MQTT_SERVER, port=1883, user=None, password=None, keepalive=3600, ssl=False, ssl_params={})
+# 0.5 Sekunde warten
+time.sleep(0.5)
+# Verbinden mit MQTT
+mqttClient.connect()
+
+# Auf dem Display wird der Erfolg der Verbindung angezeigt
+display.fill(0)
+display.text("Erfolgreich!", 0, 0)
+display.show()
+time.sleep(2)
--- main.py
+++ main.py
@@ -9,37 +9,46 @@ Hardware:
 - 1*LEDs Gruen
 - 1*LEDs Gelb
 - 1*LEDs Rot
+- 1*Buzzer
 - 1*MQ135 Gassensor
 - 1*Breadboard
 - Verdrahtungsbruecken
 
-Gassensor = Pin 36
-Led Rot = Pin 13
-Led Gelb =
-Led Grün =
+- Gassensor = Pin 36
+- Led Grün = Pin 13
+- Led Gelb = Pin 17
+- Led Rot = Pin 23
+- Buzzer = Pin 39
 
 Bibliotheken:
--machine (integriert in Micropython)
--time (integriert in Micropython)
--math (integriert in Micropython)
+- machine (integriert in Micropython)
+- time (integriert in Micropython)
+- math (integriert in Micropython)
+- json (integriert in Micropython)
+- network (integriert in Micropython)
+- MQTTClient (integriert in Micropython)
 """
-#-------------------------- Bibliotheken ------------------------------
-from machine import Pin, ADC
-import time
+from funktionen import ppm_json
+from funktionen import luft_gut
+from funktionen import luft_mittel
+from funktionen import luft_schlecht
+
 #---------------------------- Programm --------------------------------
 while True:
-    adc = ADC(Pin(36))
-    adc.atten(ADC.ATTN_11DB) # Einstellen des ADC-Verstärkungsfaktors auf 11 dB
-
-    # Lesen Sie den analogen Wert
-    analogwert = adc.read()
-
-    # Drucken Sie den Wert auf der Konsole
-    print(analogwert)
+    # Den Analogwert vom Sensor lesen
+    ppm = analog.read()
+    # JSON-Funktion ausführen
+    ppm_json()
     
-    # Umwandeln des Analogwerts in eine Spannung
-   # spannung = analogwert * 0.0078125
-
-    # Drucken Sie die Spannung auf der Konsole
-   # print(spannung)
+    # Wenn der PPM-Wert unter 1000 liegt, die gute Luftqualität Funktion verwenden
+    if ppm <= 1000:
+        luft_gut()
+    # Wenn der PPM-Wert zwischen 1000 und 1599 liegt, die mittlere Luftqualität Funktion verwenden
+    if ppm >= 1000 and ppm <=1599:
+        luft_mittel()
+    # Wenn der PPM-Wert unter 1000 liegt, die schlechte Luftqualität Funktion verwenden
+    if ppm >= 1600:
+        luft_schlecht()
+    
+    # Jede Sekunde den Wert aktualisieren
     time.sleep(1)
